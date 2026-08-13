50219f4430b45ccf1492049494cc34aaea90b2f5
Ananthaprakash <ananthaprakashb@gmail.com>
2026-08-13 10:37:50 -0700

    Compose app root

---
+++ src/AppRoot.tsx
@@ -0,0 +1,11 @@
+import CristovaoApp from './CristovaoApp';
+import SourceImpactDemoControl from './SourceImpactDemoControl';
+
+export default function AppRoot() {
+  return (
+    <>
+      <SourceImpactDemoControl />
+      <CristovaoApp />
+    </>
+  );
+}
