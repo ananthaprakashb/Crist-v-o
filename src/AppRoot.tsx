82466f9a54e76795acd632ff64637ceff5478ea7
Ananthaprakash <ananthaprakashb@gmail.com>
2026-08-13 13:47:43 -0700

    Mount checkpoint comparison panel

--- src/AppRoot.tsx
+++ src/AppRoot.tsx
@@ -1,11 +1,13 @@
 import CristovaoApp from './CristovaoApp';
 import SourceImpactDemoControl from './SourceImpactDemoControl';
 import TimelinePreview from './TimelinePreview';
+import CheckpointPanel from './CheckpointPanel';
 
 export default function AppRoot() {
   return (
     <>
       <SourceImpactDemoControl />
+      <CheckpointPanel />
       <TimelinePreview />
       <CristovaoApp />
     </>
