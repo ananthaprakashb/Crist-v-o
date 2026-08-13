e60c1d8ea5ad12d0b7d20e8f0734681054609b0f
Ananthaprakash <ananthaprakashb@gmail.com>
2026-08-13 10:38:30 -0700

    Add impact-aware app entry

---
+++ src/mainWithImpact.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import AppRoot from './AppRoot';
+import './styles.css';
+import './sourceIntelligence.css';
+import './intelligence.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <AppRoot />
+  </StrictMode>,
+);
